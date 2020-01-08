c5d6fa5fe668123b0c96aaa6dd754c9e79fcb00c
Kalela <philipkalela@gmail.com>
2020-01-08 17:57:10 +0300

    initial commit

---
+++ lib/Discover.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class DiscoverPage extends StatefulWidget {
+  DiscoverPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _DiscoverPageState createState() => _DiscoverPageState();
+}
+
+class _DiscoverPageState extends State<DiscoverPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Discover Layout', style: TextStyle(fontSize: 36.0),),
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import './Discover.dart';
+import './Library.dart';
+import './Search.dart';
+
+void main() => runApp(MyMusicApp());
+
+class MyMusicApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyMusicApp>{
+
+  int _selectedPage = 0;
+  final _pageOptions = [
+    DiscoverPage(title: "Flutter app home",),
+    LibraryPage(),
+    SearchPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Music For All App'),),
+        body: _pageOptions[_selectedPage],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedPage,
+          onTap: (int index) {
+            setState(() {
+              _selectedPage = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard),
+              title: Text('Discover')
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.library_music),
+                title: Text('Library')
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                title: Text('Search')
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
